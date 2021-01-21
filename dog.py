69f5d1c1ac945fd6f596a765715c8412df7ba2c9
Mihail Marian <mihailmarian12@gmail.com>
2021-01-21 17:12:07 +0000

    woof woof

---
+++ dog.py
@@ -0,0 +1,23 @@
+"""woof"""
+
+
+class Dog:
+    """woof woof"""
+
+    def __init__(self, name, age):
+        """initialise name & age attributes"""
+        self.name = name.title()
+        self.age = age
+
+    def sit(self):
+        """simulate dog sitting"""
+        print(self.name + " is now sitting.")
+
+    def roll_over(self):
+        """simulate roll over"""
+        print(self.name + " is now rolling over!")
+
+
+my_dog = Dog("precious", 10)
+
+print("My dog's name is " + my_dog.name)
